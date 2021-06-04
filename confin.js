019d3cfc9e81f33da2aeba55c8b5f74c4c44ac35
sparta9974 <85248604+sparta9974@users.noreply.github.com>
2021-06-04 14:05:54 +0300

    Create confin.js

---
+++ confin.js
@@ -0,0 +1,15 @@
+/* Your Bot Default Prefix For Every Server */
+
+exports.Default_Prefix = "";
+
+/* Your Bot All Embeds Color */
+
+exports.Color = "";
+
+/* Your Bot Token (Secret) */
+
+exports.Token = "";
+
+/* Bot's Owner ID */
+
+exports.OwnerID = "";
